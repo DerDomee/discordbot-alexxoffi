317952a293d7247d98c9bdb0c12a46f4158e33e2
Dominik Riedig <me@dominikriedig.de>
2020-07-02 01:48:42 +0200

    Implement command set_bot_prefix

--- resources/dcbot/commands/set_bot_prefix.py
+++ resources/dcbot/commands/set_bot_prefix.py
@@ -1,10 +1,22 @@
 from resources.dcbot import botcommon
+from resources.database import dbcommon
 
 
 @botcommon.requires_perm_level(level=botcommon.key_permlevel_admin)
 @botcommon.requires_channel([botcommon.key_bot_adminchannel])
 async def invoke(message, arg_stack, botuser):
-    print("##################################")
-    print("# CAUTION! UNIMPLEMENTED FUNCTION!")
-    print("##################################")
-    return True
+    if len(arg_stack) == 1:
+        return False
+    if len(arg_stack) == 2:
+        if arg_stack[1] == "help":
+            pass
+        elif len(arg_stack[1]) == 1:
+            allowed_prefixes = "!§$%&/=?.:#+*|\\"
+            if not arg_stack[1] in allowed_prefixes:
+                return False
+            dbcommon.set_bot_setting(
+                botcommon.key_bot_prefix,
+                arg_stack[1])
+            return True
+        else:
+            return False
