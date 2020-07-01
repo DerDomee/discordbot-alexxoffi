2faf7a952e753ba7f6007c87a5235e77f692bd7a
Dominik Riedig <me@dominikriedig.de>
2020-07-02 01:49:00 +0200

    Implement command set_log_channel

--- resources/dcbot/commands/set_log_channel.py
+++ resources/dcbot/commands/set_log_channel.py
@@ -1,10 +1,24 @@
 from resources.dcbot import botcommon
+from resources.database import dbcommon
 
 
 @botcommon.requires_perm_level(level=botcommon.key_permlevel_admin)
 @botcommon.requires_channel([botcommon.key_bot_adminchannel])
 async def invoke(message, arg_stack, botuser):
-    print("##################################")
-    print("# CAUTION! UNIMPLEMENTED FUNCTION!")
-    print("##################################")
+
+    async def err1():
+        await message.channel.send(transget(
+            "command_set_admin_channel_error_arg1",
+            botuser.user_pref_lang))
+
+    if len(arg_stack) < 2:
+        await err1()
+        return False
+    elif not arg_stack[1].startswith("<#") or not arg_stack[1].endswith(">"):
+        await err1()
+        return False
+
+    channelid = int(arg_stack[1].lstrip("<#").rstrip(">"))
+    dbcommon.set_bot_setting(botcommon.key_bot_logchannel, channelid)
+    await message.add_reaction("✅")
     return True
