71a9c03764d3e25e6c46bc9b23624d16c7cb7c58
Dominik Riedig <me@dominikriedig.de>
2020-07-02 03:27:21 +0200

    Implement command 'stop' (Needs suitable logging)

--- resources/dcbot/commands/stop.py
+++ resources/dcbot/commands/stop.py
@@ -1,8 +1,9 @@
 from resources.dcbot import botcommon
+from resources.dcbot import client
 
 
 @botcommon.requires_perm_level(level=botcommon.key_permlevel_owner)
 @botcommon.requires_channel([botcommon.key_bot_adminchannel])
 async def invoke(message, arg_stack, botuser):
-    print("Print from inside the stop function")
+    await client.logout()
     return True
