c45bd6de57293c61bc1e7b25728d484c76dc1b52
Dominik Riedig <me@dominikriedig.de>
2020-07-02 03:25:33 +0200

    Implement command 'language'

---
+++ resources/dcbot/commands/language.py
@@ -0,0 +1,38 @@
+from resources.dcbot import botcommon
+from resources.translation import transget
+from resources.database import sqlsession
+
+
+@botcommon.requires_perm_level(level=botcommon.key_permlevel_restricted)
+@botcommon.requires_channel([
+    botcommon.key_bot_adminchannel,
+    botcommon.key_bot_userchannel])
+async def invoke(message, arg_stack, botuser):
+
+    supported_langs = ["en", "de", "fr"]
+
+    if len(arg_stack) == 1:
+        return False
+    if len(arg_stack) == 2:
+        if arg_stack[1] == "help":
+            # TODO: print help message
+            pass
+        elif arg_stack[1] == "get":
+            await message.channel.send(transget(
+                'command.language.get.text',
+                botuser.user_pref_lang).format(
+                    lang=botuser.user_pref_lang)
+                )
+            pass
+    if len(arg_stack) == 3:
+        if arg_stack[1] == "set":
+            if arg_stack[2] in supported_langs:
+                botuser.user_pref_lang = arg_stack[2]
+                sqlsession.commit()
+                await message.channel.send(transget(
+                    'command.language.set.success',
+                    botuser.user_pref_lang).format(
+                        lang=botuser.user_pref_lang)
+                    )
+            else:
+                print("provided lang not supported")
