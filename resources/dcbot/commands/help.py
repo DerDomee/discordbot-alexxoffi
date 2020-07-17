c3bed98f44f8dbd990baf13c564a673d2048f327
Dominik Riedig <me@dominikriedig.de>
2020-07-17 03:12:34 +0200

    Implemented something in help-command Need to push as I am going to sleep now and on vacation tomorrow.

--- resources/dcbot/commands/help.py
+++ resources/dcbot/commands/help.py
@@ -1,11 +1,32 @@
-from resources.dcbot import botcommon
-from resources.translation import transget
+from discord import Embed
 from resources.database import sqlsession
+from resources.dcbot import botcommon
+from resources.dcbot import commands
+from resources.translation import transget
+
+CMD_METADATA = {
+    'required_permlevel': botcommon.key_permlevel_restricted,
+    'required_channels': [botcommon.key_bot_adminchannel,
+                          botcommon.key_bot_userchannel],
+    'command_syntax': "[command_name]"}
 
 
-@botcommon.requires_perm_level(level=botcommon.key_permlevel_restricted)
-@botcommon.requires_channel([
-    botcommon.key_bot_adminchannel,
-    botcommon.key_bot_userchannel])
+async def _do_general_help(message, arg_stack, botuser):
+    helpmessage = "{help_general_title}\n\n" \
+                  "Usage: {bot_shortprefix}help or" \
+                  "{bot_shortprefix}help <command>\n\n"
+    await message.channel.send("```" + helpmessage + "```")
+
+
+async def _do_specific_help(message, arg_stack, botuser):
+    await_message.channel.send("Hier kommt noch spezifische Befehl-Hilfe!")
+
+
+@botcommon.requires_perm_level(level=CMD_METADATA['required_permlevel'])
+@botcommon.requires_channel(CMD_METADATA['required_channels'])
 async def invoke(message, arg_stack, botuser):
-    await message.channel.send("Hier kommt später Hilfe.")
+    if len(arg_stack) is 1:
+        await _do_general_help(message, arg_stack, botuser)
+        print(botcommon.registered_bot_commands)
+    else:
+        await _do_specific_help(message, arg_stack, botuser)
