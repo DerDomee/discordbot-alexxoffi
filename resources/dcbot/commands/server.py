c9a23ab5aeb7f623aa7ed11d6e31f930c70c0018
Dominik Riedig <me@dominikriedig.de>
2020-08-12 00:58:28 +0200

    Add command "server" that prints server information

---
+++ resources/dcbot/commands/server.py
@@ -0,0 +1,58 @@
+import os
+from resources.dcbot import botcommon
+from resources.dcbot import client
+from resources.database import dbcommon
+from resources.translation import transget
+from discord import Embed, Status
+
+CMD_METADATA = {
+    'required_permlevel': botcommon.key_permlevel_user,
+    'required_channels': [botcommon.key_bot_userchannel,
+                          botcommon.key_bot_adminchannel],
+    'command_syntax': ""}
+
+
+@botcommon.requires_perm_level(level=CMD_METADATA['required_permlevel'])
+@botcommon.requires_channel(CMD_METADATA['required_channels'])
+async def invoke(message, arg_stack, botuser):
+    embed = Embed(
+        title=message.guild.name,
+        description=transget(
+            'command.server.embed.desc',
+            botuser.user_pref_lang),
+        color=botcommon.key_color_info)
+    embed.set_thumbnail(url=message.guild.icon_url)
+    created = message.guild.created_at.date()
+
+    embed.add_field(
+        name=transget(
+            'command.server.embed.field.creation_date.title',
+            botuser.user_pref_lang),
+        value=f"{created.day}.{created.month}.{created.year}")
+
+    embed.add_field(
+        name=transget(
+            'command.server.embed.field.owner.title',
+            botuser.user_pref_lang),
+        value=message.guild.owner.mention
+        )
+
+    onlinecount = sum(
+        m.status != Status.offline for m in message.guild.members)
+    embed.add_field(
+        name=transget(
+            'command.server.embed.field.members.title',
+            botuser.user_pref_lang).format(
+                member_count=message.guild.member_count),
+        value=transget(
+            'command.server.embed.field.members.value',
+            botuser.user_pref_lang).format(
+                member_count=onlinecount)
+        )
+
+    footertext = "Requested by " + str(message.author.name) + "#" \
+        + str(message.author.discriminator) + " (" \
+        + str(message.author.id) + ")"
+    embed.set_footer(text=footertext)
+
+    await message.channel.send(embed=embed)
