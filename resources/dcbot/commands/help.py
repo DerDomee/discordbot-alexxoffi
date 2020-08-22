b577b806fd455624f2c20d008a5b99ddb5e3353c
Dominik Riedig <me@dominikriedig.de>
2020-08-22 16:08:29 +0200

    In help command: Use markdown syntax highlighting for better readability

--- resources/dcbot/commands/help.py
+++ resources/dcbot/commands/help.py
@@ -14,14 +14,14 @@ CMD_METADATA = {
 async def _do_general_help(message, arg_stack, botuser):
     helpmsg = transget(
         'command.help.general_help.header',
-        botuser.user_pref_lang) + "\n```"
+        botuser.user_pref_lang) + "\n```md\n"
     for command in botcommon.registered_bot_commands:
         cmdimport = importlib.import_module(
             '.' + command,
             'resources.dcbot.commands')
         cmdmeta = cmdimport.CMD_METADATA
         if botuser.user_permission_level >= cmdmeta['required_permlevel']:
-            helpmsg += command + " - " + transget(
+            helpmsg += "<" + command + "> " + transget(
                 'command.' + command + '.meta.description',
                 botuser.user_pref_lang) + "\n"
     helpmsg += "```"
@@ -29,7 +29,24 @@ async def _do_general_help(message, arg_stack, botuser):
 
 
 async def _do_specific_help(message, arg_stack, botuser):
-    await message.channel.send("Hier kommt noch spezifische Befehl-Hilfe!")
+    try:
+        cmdimport = importlib.import_module(
+            '.' + arg_stack[1],
+            'resources.dcbot.commands')
+    except ModuleNotFoundError as e:
+        # TODO: Translate this
+        await message.channel.send("Command not found")
+    else:
+        try:
+            if botuser.user_permission_level >= \
+                    cmdimport.CMD_METADATA['required_permlevel']:
+                await cmdimport.print_help(message, arg_stack, botuser)
+            else:
+                await message.channel.send(
+                    "You have no permission to get help for this command.")
+        except Exception as e:
+            await message.channel.send(
+                "This command has no function to print help.")
 
 
 @botcommon.requires_perm_level(level=CMD_METADATA['required_permlevel'])
