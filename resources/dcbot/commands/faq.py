af91edc242bb366a3ec916bf4bb17a1a90e374ed
Dominik Riedig <me@dominikriedig.de>
2020-08-26 23:54:29 +0200

    Add FAQ Command (awaiting correct argument vector for implementation)

---
+++ resources/dcbot/commands/faq.py
@@ -0,0 +1,14 @@
+from discord import Embed
+from resources.dcbot import botcommon
+from resources.dcbot import client
+from resources.dcbot.botcommon import trytolog
+
+CMD_METADATA = {
+    'required_permlevel': botcommon.key_permlevel_moderator,
+    'required_channels': [botcommon.key_bot_adminchannel]}
+
+
+@botcommon.requires_perm_level(level=CMD_METADATA['required_permlevel'])
+@botcommon.requires_channel(CMD_METADATA['required_channels'])
+async def invoke(message, arg_stack, botuser):
+    return True
