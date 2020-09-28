3a33cda7e21bb335c3ee11548c458dca9a5a2801
Dominik Riedig <me@dominikriedig.de>
2020-09-28 20:22:33 +0200

    Comments describing what every function should do.
    Awaiting approval by server administratives to use this implementation.

--- resources/dcbot/events/on_voice_state_update.py
+++ resources/dcbot/events/on_voice_state_update.py
@@ -6,7 +6,8 @@ from resources.dcbot.events.voice_events import voicecommon
 
 @client.event
 async def on_voice_state_update(member, before, after):
-    # Should create a new public channel
+
+    # When new channel is the "Create new Public Talk" channel
     if after.channel is not None and after.channel.id == 679801626747469960:
         channel_obj = await new_voice.create_public(member, before, after)
         botcommon.bot_voice_channels.append(channel_obj)
@@ -14,7 +15,8 @@ async def on_voice_state_update(member, before, after):
             await voicecommon.move_to(member, channel_obj)
         except Exception:
             await voicecommon.delete_channel(channel_obj)
-    # Should create a new private channel
+
+    # When new channel is the "Create new Private Talk" channel
     if after.channel is not None and after.channel.id == 759136424661876766:
         channel_obj = await new_voice.create_private(member, before, after)
         botcommon.bot_voice_channels.append(channel_obj)
@@ -23,10 +25,13 @@ async def on_voice_state_update(member, before, after):
         except Exception:
             await voicecommon.delete_channel(channel_obj)
 
-    # Check if old channel is dynamic
+    # When old channel is dynamic
     if voicecommon.get_channel_obj(before.channel) is not None:
         channel_obj = voicecommon.get_channel_obj(before.channel)
-        await member.remove_roles(channel_obj['role'])
+        if channel_obj['type'] == "public":
+            # Only take away role when channel is public.
+            # Roles for private channels are taken away by commands only.
+            await member.remove_roles(channel_obj['role'])
         # Remove channel if it is empty now
         if len(channel_obj['voicechannel'].members) is 0:
             await voicecommon.delete_channel(channel_obj)
@@ -34,4 +39,7 @@ async def on_voice_state_update(member, before, after):
     # Check if new channel is dynamic
     if voicecommon.get_channel_obj(after.channel) is not None:
         channel_obj = voicecommon.get_channel_obj(after.channel)
-        await member.add_roles(channel_obj['role'])
+        if channel_obj['type'] == "public":
+            # Only give role when channel is public.
+            # Roles for private channels are given by commands only.
+            await member.add_roles(channel_obj['role'])
