9c0547da2153a10e729167d9d662df347ee9f123
Dominik Riedig <me@dominikriedig.de>
2021-02-17 23:14:32 +0100

    Make log messages more clear when stopping bot

--- src/dcbot/commands/stop.py
+++ src/dcbot/commands/stop.py
@@ -20,15 +20,15 @@ async def invoke(message, arg_stack, botuser):
     botcommon.hypixel_api.stop()
     botcommon.hypixel_api.join()
 
-    print("Stopping Challenge Scheduler")
+    print("Stopping Challenge Scheduler...")
     botcommon.challenge_scheduler.stop()
     botcommon.challenge_scheduler.join()
 
-    print("Closing and deleting active dynamic voice channels")
+    print("Closing and deleting active dynamic voice channels...")
     for vchannel_obj in botcommon.bot_voice_channels:
         await voicecommon.delete_channel(vchannel_obj)
 
-    print("Log close action to log channel")
+    print("Log close action to log channel...")
     embed = Embed(
         title="Bot shuts down",
         description="Due to an admin command the bot shuts down now.",
@@ -39,7 +39,7 @@ async def invoke(message, arg_stack, botuser):
     embed.set_footer(text=footertext)
     await trytolog(message, arg_stack, botuser, embed)
 
-    print("Logout the bot client and return from discordpy")
+    print("Logout the bot client and return from discordpy...")
     await client.logout()
     print("Bot stopped successfully!")
     return True
