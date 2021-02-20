382dff2792bc91d0f7549eaae7bdc30fbe9f1768
Dominik Riedig <me@dominikriedig.de>
2021-02-20 15:34:51 +0100

    After loading tracked events, update the embeds

--- src/dcbot/events/on_ready.py
+++ src/dcbot/events/on_ready.py
@@ -15,11 +15,13 @@ async def on_ready():
     print("Initializing the bot...")
     print(f"Setting main guild to {str(client.guilds[0])}")
     botcommon.main_guild = client.guilds[0]
+
     # Place all commands in a register used for the help command
     print("Getting all enabled commands...")
     for importer, modname, ispkg in pkgutil.iter_modules(commands.__path__):
         if not str(modname).startswith('_'):
             botcommon.registered_bot_commands.append(modname)
+
     # Place all message processors in a register to call everyone of them
     # on every message ever sent
     print("Getting all enabled message processors...")
@@ -27,14 +29,21 @@ async def on_ready():
             messageprocessors.__path__):
         if not str(modname).startswith('_'):
             botcommon.registered_message_processors.append(modname)
+
     print("Starting Hypixel API...")
     botcommon.hypixel_api = hypixelv2.SkyblockAPI(
         os.getenv('DD_HYPIXEL_API_KEY', ''))
     botcommon.hypixel_api.start()
-    print("Starting Challenge Event scheduler")
+
+    print("Starting Challenge Event scheduler...")
     botcommon.challenge_scheduler = botcommon.ChallengeScheduler()
     botcommon.challenge_scheduler.start()
+
+    print("Load previously tracked challenge events into the scheduler...")
     botcommon.challenge_scheduler.load_all_tracked()
+    for challenge in botcommon.challenge_scheduler.getAllTasks():
+        await challenge.update_challenge_embed()
+
     # Log the ready-state of the bot in the console.
     message = transget("dcbot.readymessage").format(
         client=client.user)
